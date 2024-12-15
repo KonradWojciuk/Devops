db16f659fb545b7b7c89b23394f569536b57b788
KonradWojciuk <konrad.wojciuk123@gmail.com>
2024-12-15 14:19:21 +0000

    Config Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.Extensions.DependencyInjection;
+using WeatherApp.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<WeatherService>();
+
 var app = builder.Build();
 
 // Config app to listen on all interfaces
 app.Urls.Add("http://0.0.0.0:5000");
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/weather/{city}", async (string city, WeatherService weatherService) =>
+{
+    var weather = await weatherService.GetWeatherAsync(city);
+    return weather is not null ? Results.Ok(weather) : Results.NotFound();
+});
+
+app.UseStaticFiles();
 
 app.Run();
