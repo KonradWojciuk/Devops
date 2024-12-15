7a6e330963d2843a6f945696404dfb59ea9e4e78
KonradWojciuk <konrad.wojciuk123@gmail.com>
2024-12-15 13:02:24 +0000

    Config localhost

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Config app to listen on all interfaces
+app.Urls.Add("http://0.0.0.0:5000");
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
